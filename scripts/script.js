3860c3a5ee7566ea73c9d2719afb7fb947a00797
Tobi <47112773+Tobylarry@users.noreply.github.com>
2023-09-17 21:07:55 -0700

    new update

--- scripts/script.js
+++ scripts/script.js
@@ -85,12 +85,14 @@ minus.addEventListener('click', function(){
     else{
         value--;
         qty.innerHTML = value;
+        valaue = 0;
     }
 })
 
 addition.addEventListener('click', function(){
     value++;
     qty.innerHTML = value;
+    value = 0;
 })
 
 
@@ -162,6 +164,20 @@ add.addEventListener('click', function(){
         }
     }
     localStorage.setItem("cart", JSON.stringify(cart));
+
+    
+//get order price total
+function getTotal(){
+    let temp = cart.map(function(item){
+        return parseFloat(item.price);
+    })
+
+    let sum = temp.reduce(function(prev, next){
+        return prev + next;
+    }, 0);
+
+    console.log(sum, 'hero');
+}
 
 
     console.log(localStorage.getItem('cart'))
