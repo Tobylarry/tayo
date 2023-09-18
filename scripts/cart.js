aea1dcd2c530702f5d8866df3bb75fe2ceeee9b6
Tobi <47112773+Tobylarry@users.noreply.github.com>
2023-09-18 13:47:19 -0700

    Update cart.js

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,50 +1,45 @@
 let qy = document.querySelector('.qty');
 let clearCart = document.querySelector('.cc');
-let subTotal = document.querySelector('.subTotal');
 
-    let obj = JSON.parse(localStorage.getItem('cart'))
 
 if(localStorage.getItem('cart')){
     let obj = JSON.parse(localStorage.getItem('cart'))
-    console.log(obj);
+    console.log(obj)
    let showItem = obj.map((item) =>{
        return `
        <div class='it'>
        <img src="${item.image}" style='height:80px; width: 50px;' alt="image"/>
        <div>
        <p>${item.name}</p>
-       <p>${item.price}</p>
+       <p>$${item.price}</p>
        </div>
        <div class='pqt'>
         <div><label>Price</label><br>
-        <p>${'$' + item.price}</p></div>
+        <p>$50</p></div>
         <div>
         <label>Quantity</label><br>
-        <span>-</span> <p>${item.quantity}</p><span> + </span>
-        <span>-</span> <p>${item.quantity}</p><span> + </span>
+        <input type="number" placeholder="2"/><br>
         </div>
        <div>
        <label>Total</label><br>
-       <p>${'$' + (item.price * item.quantity)}</p>
+       <p>$50</p>
        </div>
        </div>
        </div>
 
        `
-       
     })
     document.querySelector('.det').innerHTML = showItem;
 }
-
 window.onload = function(){
-    if(localStorage.getItem('cart').length != 0){
+    if(localStorage.getItem('cart') == 0){
         notification.style.display = 'block';
     }
+   
 }
 
 clearCart.addEventListener('click', function(){
     location.reload();
-    localStorage.setItem('cart', '[]');
-    obj = JSON.parse(localStorage.getItem('cart'))
-    document.querySelector('.det').innerHTML = ' EMPTY CART! PLEASE PLACE AN ORDER TO PROCEED';
+    alert('cart cleared')
+    localStorage.clear()
 })
