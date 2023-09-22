eda096a1d8271aadee8fb05e92c3cf40883b4b33
Tobi <47112773+Tobylarry@users.noreply.github.com>
2023-09-21 18:43:33 -0700

    u

--- scripts/cart.js
+++ scripts/cart.js
@@ -12,7 +12,7 @@ let cartItems = JSON.parse(localStorage.getItem('cart')) //get cart  items.....
     if(cartItems.length > 0){
         let obj = JSON.parse(localStorage.getItem('cart'))
        
-        obj = obj.match(/,/g);
+        obj = obj.join('')
         console.log(obj)
 
        let showItem = obj.map((item) =>{
